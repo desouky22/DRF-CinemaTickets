fa228a95d16e7faca9326f932e012b04cbff8bba
Omar Desouky <omaressamdesouky2274@gmail.com>
2022-11-05 12:36:57 +0200

    auth

--- project/settings.py
+++ project/settings.py
@@ -125,14 +125,11 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# REST_FRAMEWORK = {
-#     "DEFAULT_AUTHENTICATION_CLASSES": [
-#         "rest_framework.authentication.BasicAuthentication"
-#     ],
-#     "DEFAULT_PERMISSION_CLASSES": [
-#         "rest_framework.permissions.IsAuthenticated",
-#     ],
-# }
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication"
+    ],
+}
 
 # All Authentication
 # ========================
